bc0e9f412fa534a91f08662907d80813f867634d
Jere Virta <jerenius@gmail.com>
2017-12-10 14:49:56 +0200

    development

--- content/opt/start.sh
+++ content/opt/start.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-echo "Hello world 2.0"
+
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Configuring..."
+
+PKI_DIR="/etc/icinga2/pki"
+
+echo "$MASTER_IP $MASTER_HOST" >>/etc/hosts
+echo "127.0.2.1 $CLIENT_HOST" >>/etc/hosts
+
+chown nagios.nagios /etc/icinga2 -R
+
+rm -rf /etc/icinga2/conf.d/*
+
+
+
+
+
+
+
+
+
+echo "Hello world 3.0"
