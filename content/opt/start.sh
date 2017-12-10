a6fb5fd136331ffbe5c5904d769ea35091baf69f
Jere Virta <jerenius@gmail.com>
2017-12-10 15:07:22 +0200

    development

--- content/opt/start.sh
+++ content/opt/start.sh
@@ -16,8 +16,12 @@ chown nagios.nagios /etc/icinga2 -R
 rm -rf /etc/icinga2/conf.d/*
 
 
+echo "curl -k -s -u $API_USER:$API_PWD -H 'Accept: application/json' -X POST \"https://$MASTER_IP:5665/v1/actions/generate-ticket\" -d '{ \"cn\": \"$CLIENT_HOST\" }'">/opt/ticket.sh
+
+ICINGA_TICKET=`sh /opt/ticket.sh | awk -F\" '{print $12}'`
 
 
+echo $ICINGA_TICKET
 
 
 
